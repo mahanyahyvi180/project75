dbd9a2432e06fe3faedec83429db501cf3562b17
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 10:29:34 +0330

    Add files via upload

---
+++ ConsoleApp75/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 5);
+
+            Console.ReadLine();
+        }
+    }
+}
